afd0be7039773ac932279d7dbc8a70f991170ae8
Fabio Lima <44278903+fabiomicromais@users.noreply.github.com>
2023-10-17 11:18:06 -0300

    aula01

---
+++ aula01/exp02.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exp02</title>
+</head>
+<body>
+    <?php
+        echo 'teste1 <br>';
+        echo 'teste2'; echo '<br>';
+        echo 'meu nome é ' . '  maria das onças ' . '<br>';
+    ?>
+</body>
+</html>
---
+++ aula01/index.php
@@ -0,0 +1,25 @@
+<?php
+   echo ' Teste ipressao php ';
+
+echo '<br>';
+echo '<hr>';
+
+?>
+<?= " Mas uma nova linha agora " ?>
+
+<?php
+
+// echo "comentario de uma linha ";
+
+/* comentario de varias linhas
+    nova linha agora
+    nova linha agora
+*/
+
+   echo "teste";
+?>
+
+
+
+
+
